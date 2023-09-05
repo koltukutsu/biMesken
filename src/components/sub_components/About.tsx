92dd02a5eb291bbfdd0e249c4ea73078afa86c98
koltukustu <l6918040@std.yildiz.edu.tr>
2023-09-05 12:37:09 +0000

    Update; updated the educators

--- src/components/sub_components/About.tsx
+++ src/components/sub_components/About.tsx
@@ -1,7 +1,7 @@
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import config from './index.json';
+import config from "./index.json";
 
 interface PersonCardProps {
   name: string;
@@ -12,29 +12,36 @@ interface PersonCardProps {
 
 function PersonCard({ name, position, photo, linkedin }: PersonCardProps) {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden">
+    <div className="overflow-hidden rounded-lg bg-white shadow-md">
       <img
-        className="filter grayscale w-full object-cover sm:h-60 md:h-72 lg:h-80"
+        className="w-full object-cover grayscale filter sm:h-60 md:h-72 lg:h-80"
         src={photo}
         alt={name}
       />
-      <div className="p-4">
-        <a
-          href={linkedin}
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold text-gray-800 hover:text-primary"
-        >
-          {' '}
-          <div className="flex items-center mb-2">
-            <FontAwesomeIcon icon={faLinkedin} className="mr-2 h-4" />
+      {linkedin ? (
+        <div className="p-4">
+          <a
+            href={linkedin}
+            target="_blank"
+            rel="noreferrer"
+            className="font-bold text-gray-800 hover:text-primary"
+          >
+            {" "}
+            <div className="mb-2 flex items-center">
+              <FontAwesomeIcon icon={faLinkedin} className="mr-2 h-4" />
 
-            {name}
-          </div>
-        </a>
+              {name}
+            </div>
+          </a>
 
-        <p className="text-gray-600">{position}</p>
-      </div>
+          <p className="text-gray-600">{position}</p>
+        </div>
+      ) : (
+        <div className="p-4 ">
+           <p className="font-bold text-gray-800">{name}</p> 
+           <p className="text-gray-600">{position}</p>
+        </div>
+      )}
     </div>
   );
 }
@@ -43,14 +50,14 @@ const About = () => {
   const { about } = config;
   // const { logo, name: companyName } = company;
   const { sections } = about;
-  
+
   return (
     <div className="container mx-auto py-6 lg:px-48" id="team">
       {/* <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-black pb-4">
         Ekibimiz
       </h1> */}
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4 m-2">
+      <div className="m-2 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4">
         {sections.map((person) => (
           <PersonCard
             key={person.alt}
@@ -63,6 +70,5 @@ const About = () => {
       </div>
     </div>
   );
-
 };
 export default About;
